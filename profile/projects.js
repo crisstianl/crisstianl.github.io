1a1336e73368d319c37de18d3c790a08d173b43d
cristian-adrian.lucaci@eon-romania.ro <cristianlucaci@macbook0101.local>
2020-05-04 22:13:20 +0300

    Project update screenshots

--- profile/projects.js
+++ profile/projects.js
@@ -1,21 +1,45 @@
 const PROJECTS = [
     {
         name : "Priceaware",
-        description : "Price aggregator website for most common online stores in Romania, such as Emag, Altex, Evomag, Olx.",
+        description : "Price aggregator website for most common online stores in Romania, such as Emag, Altex, Cell, Evomag, Olx.",
         stack : "Python 3.7, Django 4, Beautifulsoup 4, PhantomJS",
-        source : "https://github.com/crisstianl/priceaware"
+        source : "https://github.com/crisstianl/priceaware",
+        screenshots : [
+            "images/priceaware_search_p1.png",
+            "images/priceaware_search_p2.png",
+            "images/priceaware_search_p3.png",
+            "images/priceaware_cart.png",
+            "images/priceaware_stores.png",
+            "images/priceaware_settings.png"
+        ]
     },
     {
-        name : "Open Weather",
-        description : "Todays weather forecast in Europe, America, Asia, Africa and Australia based on Open Weather.",
-        stack : "OpenWeatherMap API, Google Maps, Javascript, Ajax, SpringBoot",
-        source : "https://github.com/crisstianl/weather-service"
+        name : "Openweather",
+        description : "Weather forecast in Europe, America, Asia, Africa and Australia based on Open Weather services.",
+        stack : "OpenWeatherMap API, Geolocation API, Google Maps, Javascript, Ajax, Java EE, SpringBoot",
+        source : "https://github.com/crisstianl/weather-service",
+        screenshots : [
+            "images/openweather_iasi.png",
+            "images/openweather_amsterdam.png",
+            "images/openweather_guatemala.png",
+            "images/openweather_san_jose.png",
+            "images/openweather_los_angeles.png",
+            "images/openweather_targu_mures.png",
+        ]
     },
     {
         name : "Paweo",
         description : "Stream, playlist and share your favorite videos on Youtube using this website.",
         stack : "Youtube API, PopcornJS, Javascript, JQuery, IndexDB",
-        source : "https://github.com/crisstianl/Paweo"
+        source : "https://github.com/crisstianl/Paweo",
+        screenshots : [
+            "images/paweo_trending.png",
+            "images/paweo_trending_more.png",
+            "images/paweo_topic.png",
+            "images/paweo_topic_more.png",
+            "images/paweo_search_keyword.png",
+            "images/paweo_playlist_export.png",
+        ]
     },
     {
         name : "Computer Algorithms",
